f3cb4f3f201d281176b4fcf6fdba2780550fc3c1
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-12-02 19:42:46 -0500

    Added bumpers

--- ball/ball.gd
+++ ball/ball.gd
@@ -13,3 +13,13 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body):
+	var bumper := body as Bumper
+	if bumper:
+		print("bumping")
+		var dir := (global_position - bumper.global_position).normalized()
+		var magnetude := linear_velocity.length()
+		linear_velocity = Vector2.ZERO
+		apply_central_impulse(dir * magnetude * 3)
